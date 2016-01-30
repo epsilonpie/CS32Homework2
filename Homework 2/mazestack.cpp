2fba5920446993e55c1eef55181a8bc156635b32
epsilonpie <suresh.gopikrishnan@gmail.com>
2016-01-30 14:07:16 -0800

    Coord class added

--- Homework 2/mazestack.cpp
+++ Homework 2/mazestack.cpp
@@ -2,3 +2,13 @@
 #include <string>
 #include <stack>
 
+class Coord
+{
+public:
+    Coord(int rr, int cc) : m_r(rr), m_c(cc) {}
+    int r() const { return m_r; }
+    int c() const { return m_c; }
+private:
+    int m_r;
+    int m_c;
+};
