eb8cfa4eb3b6f4efacab587d3820988f4e642032
epsilonpie <suresh.gopikrishnan@gmail.com>
2016-01-30 14:58:35 -0800

    Initialize items

--- Homework 2/mazestack.cpp
+++ Homework 2/mazestack.cpp
@@ -18,5 +18,12 @@ private:
 bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec){
     // Return true if there is a path from (sr,sc) to (er,ec)
     // through the maze; return false otherwise
+    stack<Coord> list;
+    Coord begin(sr, sc);
+    Coord end(er, ec);
+    list.push(begin);
+    
+    
     return true;
+    
 }
