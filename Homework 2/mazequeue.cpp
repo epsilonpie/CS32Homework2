6753091a02646e743cbde97321271767e116f465
epsilonpie <suresh.gopikrishnan@gmail.com>
2016-01-30 17:35:27 -0800

    add maze queue and remove main method from maze stack

---
+++ Homework 2/mazequeue.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    string maze[10] = {
+        "XXXXXXXXXX",
+        "X........X",
+        "XX.X.XXXXX",
+        "X..X.X...X",
+        "X..X...X.X",
+        "XXXX.XXX.X",
+        "X.X....XXX",
+        "X..XX.XX.X",
+        "X...X....X",
+        "XXXXXXXXXX"
+    };
+    
+    if (pathExists(maze, 10,10, 6,4, 1,1))
+        cout << "Solvable!" << endl;
+    else
+        cout << "Out of luck!" << endl;
+}
