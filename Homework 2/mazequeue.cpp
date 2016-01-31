cd61bb22b149d7d9745f3b7dc1423761c566e236
epsilonpie <suresh.gopikrishnan@gmail.com>
2016-01-30 17:37:50 -0800

    add coord class to queue implementation

--- Homework 2/mazequeue.cpp
+++ Homework 2/mazequeue.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+class Coord
+{
+public:
+    Coord(int rr, int cc) : m_r(rr), m_c(cc) {}
+    int r() const { return m_r; }
+    int c() const { return m_c; }
+private:
+    int m_r;
+    int m_c;
+};
+
+
 int main(){
     string maze[10] = {
         "XXXXXXXXXX",
